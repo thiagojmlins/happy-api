d43f3cf75c06ec47acdac47ec602b0b7df10d410
Thiago Lins <thiagojmlins@gmail.com>
2020-10-14 20:29:04 -0300

    create orphanage entity

--- src/database/migrations/1602676589529-create_orphanages_table.ts
+++ src/database/migrations/1602676589529-create_orphanages_table.ts
@@ -39,6 +39,10 @@ export class createOrphanagesTable1602676589529 implements MigrationInterface {
             type: "text",
           },
           {
+            name: "opening_hours",
+            type: "varchar",
+          },
+          {
             name: "open_on_weekends",
             type: "boolean",
             default: false,
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,37 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "./models/Orphanage";
 import "./database/connection";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello World!" });
+app.use(express.json());
+
+app.post("/orphanages", async (request, response) => {
+  console.log(request.body);
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+  return response.json({ message: "Orphanage saved!" });
 });
 
 app.listen(3334);
